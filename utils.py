2b34a902d4e5b1939fab7e85e0771f2859a8b416
samadii <77415087+samadii@users.noreply.github.com>
2021-07-03 00:11:40 +0430

    Update utils.py

--- utils.py
+++ utils.py
@@ -49,7 +49,7 @@ class Msg:
     zip = "Send the files you want to compress, and at the end send /stopzip after all the files have been downloaded.\n" \
           "\n\nNote: due to upload limit, the total size of the file(s) can be up to 2GB."
     too_big = "Note: due to upload limit, the total size of the file(s) can be up to 2GB."
-    too_much = "Note: the total number of the files can be up to 500, not over than 500"
+    too_much = "Note: the total number of the files can be up to 500, not over 500"
     send_zip = "Send /zip to compress the files"
     zipping = "start compressing {} files..."
     uploading = "uploading archive..."
